bf16f616b76b0822e6c8a69806d62ccc23866709
BakedPotatoLord <918780@dpsk12.net>
2022-06-11 11:21:38 -0600

    moved some files around

--- discordbot.py
+++ discordbot.py
@@ -95,9 +95,9 @@ async def on_message(message):
     
     if "s my fortune" in message.content.lower() or message.content == "./fortune":
         if (random.randint(0, 100) < 50):
-            await message.channel.send(texts("misfortunes.txt"))
+            await message.channel.send(texts("assets/textFiles/misfortunes.txt"))
         else:
-            await message.channel.send(texts("fortunes.txt"))
+            await message.channel.send(texts("assets/textFiles/fortunes.txt"))
     await client.process_commands(message)
 
 @client.command()
@@ -109,7 +109,7 @@ async def owner(ctx):
 @client.command(hidden=True)
 async def nice(ctx):
     if int(ctx.author.id not in niceto):
-        file = open('niceto.who', 'w+')
+        file = open('assets/textFiles/niceto.who', 'w+')
         lines = file.read()
         file.write(lines+ctx.author.id+"\n")
         on_ready()
