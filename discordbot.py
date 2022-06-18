993de2050675ffbd97c95ef69c27f1287dcb28f0
spacebonbon <happyroom877@gmail.com>
2022-06-17 21:53:55 -0600

    made it argue less

--- discordbot.py
+++ discordbot.py
@@ -79,10 +79,14 @@ async def on_ready():
     file.close()
     print(niceto)
 
+
+
 @client.event
 async def on_message(message):
-    if message.author.id == 986803208796119070:
-        await message.reply(texts("assets/textFiles/disses.4u"))
+    if message.author.id == 986803208796119070 and message.reference != None and not message.is_system:
+        tf = [True,False]
+        if random.choice(tf):
+            await message.reply(texts("assets/textFiles/disses.4u"))
     if ("./delete" in message.content.lower()):
         time.sleep(1)
         await message.delete()
